3cf4375e3f6f824fcf45ee4fbbba86a8380fd5dd
Jakob Unnebäck <jakob@folkdatorn.se>
2022-10-03 11:46:38 +0200

    Read 3 images

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,7 @@
 import { app, BrowserWindow, ipcMain } from 'electron'
+import { convertRawToRGB } from '../src/lib/BayerFilter'
+import { setSaturation } from '../src/lib/Saturation'
+import { saveImage } from './jimp'
 import { readImage } from './raw'
 
 let mainWindow: BrowserWindow | null
@@ -36,11 +39,35 @@ async function registerListeners (): Promise<void> {
    * This comes from bridge integration, check bridge.ts
    */
   ipcMain.on('message', (_, message) => {
-    console.log(message)
-    console.log('load raw...')
-    const imageData = readImage()
-    // send data to renderer
-    mainWindow?.webContents.send('message', imageData)
+    let imageData1 = null
+    let imageData2 = null
+    let imageData3 = null
+    let rgb: {r: Uint8Array, g: Uint8Array, b: Uint8Array}
+    let rgbSat: {r: number, g: number, b: number}
+
+    switch (message) {
+      case 'generateHDR':
+        imageData1 = readImage('1.arw')
+        imageData2 = readImage('2.arw')
+        imageData3 = readImage('3.arw')
+
+        mainWindow?.webContents.send('message', [imageData1, imageData2, imageData3])
+
+        // mainWindow?.webContents.send('message', imageData)
+        // const rgbSat = setSaturation({ r: rgb.r[i], g: rgb.g[i], b: rgb.b[i] }, 2.5)
+        // rgb = convertRawToRGB(imageData, 2848, 4288)
+        // for (let i = 0; i < rgb.r.length; i++) {
+        //   rgbSat = setSaturation({ r: rgb.r[i], g: rgb.g[i], b: rgb.b[i] }, 2.5)
+        //   rgb.r[i] = rgbSat.r > 255 ? 255 : rgbSat.r
+        //   rgb.g[i] = rgbSat.g > 255 ? 255 : rgbSat.g
+        //   rgb.b[i] = rgbSat.b > 255 ? 255 : rgbSat.b
+        // }
+        // saveImage(rgb, 2848, 4288)
+        break
+      case 'saveImage':
+        // saveImage(sampleImageData)
+        break
+    }
   })
 }
 
