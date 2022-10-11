a5cbfcf7561d55af9b33d989c7b2f3f6cf8e5ce4
Jakob Unnebäck <jakob@folkdatorn.se>
2022-10-11 15:57:15 +0200

    Use apply instead of loops

--- src/lib/ArrayToMatrix.ts
+++ src/lib/ArrayToMatrix.ts
@@ -3,11 +3,10 @@ import Matrix from 'ml-matrix'
 export const ArrayToMatrix = (arr: Uint8Array, width: number, height: number): Matrix => {
   const matrix = new Matrix(height, width)
   let index = 0
-  for (let i = 0; i < height; i++) {
-    for (let j = 0; j < width; j++) {
-      matrix.set(i, j, arr[index])
-      index++
-    }
-  }
+
+  matrix.apply((row, column) => {
+    matrix.set(row, column, arr[index])
+    index++
+  })
   return matrix
 }
