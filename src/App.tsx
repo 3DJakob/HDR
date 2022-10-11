67c8c2174e484c54dddfca7900cdc387190913ea
Jakob Unnebäck <jakob@folkdatorn.se>
2022-10-11 15:33:57 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react'
-import { extractSampleValuesFromImages, getActualRadiance, gsolveImage, Image, sampleChannels, ToneMapping } from './lib/HDR'
+import { extractSampleValuesFromImages, getActualRadiance, gsolveImage, Image, ToneMapping } from './lib/HDR'
 import { GlobalStyle } from './styles/GlobalStyle'
 import CanvasImage from './components/CanvasImage'
 import Matrix from 'ml-matrix'
 import { ArrayToMatrix } from './lib/ArrayToMatrix'
-import { saveImage } from '../electron/jimp'
 let appInitialized = false
 
 export const App: React.FC = () => {
@@ -12,7 +11,6 @@ export const App: React.FC = () => {
   const [status, setStatus] = React.useState('Initiated')
   // const [rawImagesData, setRawImagesData] = React.useState<[Uint8Array, Uint8Array, Uint8Array]>()
   const [RGBImagesData, setRGBImagesData] = React.useState<[Image, Image, Image]>()
-
   // const [g, setG] = React.useState<[number[], number[], number[]]>([[0, 100, 150], [0, 100, 150], [0, 100, 150]])
 
   useEffect(() => {
@@ -26,7 +24,6 @@ export const App: React.FC = () => {
       const samples = extractSampleValuesFromImages(data)
       const shutterSpeeds = [Math.log(1 / 400), Math.log(1 / 125), Math.log(1 / 20)]
       const g = gsolveImage(samples, shutterSpeeds, 100, 10, samples.length)
-      console.log(g, 'g')
 
       const r1Matrix = ArrayToMatrix(data[0].r, data[0].width, data[0].height)
       const r2Matrix = ArrayToMatrix(data[1].r, data[1].width, data[1].height)
@@ -46,8 +43,6 @@ export const App: React.FC = () => {
         new Matrix([g.gBlue])
       ]
 
-      // console.log(radianceMaps[0].get(0, 10))
-
       const red = getActualRadiance([r1Matrix, r2Matrix, r3Matrix], radianceMaps, shutterSpeeds)
       const blue = getActualRadiance([b1Matrix, b2Matrix, b3Matrix], radianceMaps, shutterSpeeds)
       const green = getActualRadiance([g1Matrix, g2Matrix, g3Matrix], radianceMaps, shutterSpeeds)
@@ -62,7 +57,6 @@ export const App: React.FC = () => {
         height: data[0].height
       }
 
-      // Send image to server
       window.Main.sendImage(JSON.stringify(image))
     })
 
@@ -74,30 +68,16 @@ export const App: React.FC = () => {
     setStatus('Loading images')
   }
 
-  const testGSolve = (): void => {
-    const res = gsolveImage(sampleChannels, [Math.log(32), Math.log(4), Math.log(1 / 4), Math.log(1 / 64)], 200, 10, 3)
-    console.log(res)
-    // setG([res.gRed, res.gGreen, res.gBlue])
-  }
-
   return (
     <>
       <GlobalStyle />
       {RGBImagesData != null && (
         <CanvasImage rgb={RGBImagesData[0]} />
       )}
-
-      {/* {rawImagesData != null && ( */}
-      {/* <GSolverHistogram raw={rawImagesData} /> */}
-      {/* )} */}
-
       <canvas ref={canvasRef} />
       <h2 style={{ color: '#fff' }}>{status}</h2>
       <button onClick={loadImages}>Generate HDR</button>
       <button onClick={() => window.Main.sendMessage('saveImage')}>Save image png</button>
-      <button onClick={testGSolve}>Gsolve</button>
-
-      {/* <button onClick={() => weight(100)}>weight</button> */}
     </>
   )
 }
--- src/lib/HDR.ts
+++ src/lib/HDR.ts
@@ -1,26 +1,5 @@
 import { Matrix, solve } from 'ml-matrix'
 
-// https://www.npmjs.com/package/ml-matrix
-
-// const addition       = Matrix.add(A, B);
-// const multiplication = A.mmul(B);
-
-// const a = [
-//   [22, 10, 2, 3, 7],
-//   [14, 7, 10, 0, 8],
-//   [-1, 13, -1, -11, 3],
-//   [-3, -2, 13, -2, 4],
-//   [9, 8, 1, -2, 4],
-//   [9, 1, -7, 5, -1],
-//   [2, -6, 6, 5, 1],
-//   [4, 5, 0, -2, 2]
-// ]
-
-// const { u, v, q } = SVD(a)
-// console.log(u)
-// console.log(v)
-// console.log(q)
-
 export function weight (x: number): number {
   let out = 0
   if (x < (255 / 2)) {
@@ -32,13 +11,6 @@ export function weight (x: number): number {
   return out
 }
 
-const sampleChannel = [255, 255, 255, 255]
-
-const sampleShutterSpeed = [
-  1 / 8000,
-  1 / 6400
-]
-
 export interface Image {
   r: Uint8Array
   g: Uint8Array
@@ -46,30 +18,6 @@ export interface Image {
   width: number
   height: number
 }
-
-const sampleImages: Image[] = [
-  {
-    r: Uint8Array.from([200, 200, 200, 200]),
-    g: Uint8Array.from([200, 200, 200, 200]),
-    b: Uint8Array.from([200, 200, 200, 200]),
-    width: 2,
-    height: 2
-  },
-  {
-    r: Uint8Array.from([100, 100, 100, 100]),
-    g: Uint8Array.from([100, 100, 100, 100]),
-    b: Uint8Array.from([100, 100, 100, 100]),
-    width: 2,
-    height: 2
-  },
-  {
-    r: Uint8Array.from([50, 50, 50, 50]),
-    g: Uint8Array.from([50, 50, 50, 50]),
-    b: Uint8Array.from([50, 50, 50, 50]),
-    width: 2,
-    height: 2
-  }
-]
 
 export function extractSampleValuesFromImages (images: Image[]): [Matrix, Matrix, Matrix] {
   const samples = 10
@@ -99,8 +47,6 @@ export function extractSampleValuesFromImages (images: Image[]): [Matrix, Matrix
 
   return [matrixR, matrixG, matrixB]
 }
-
-export const sampleChannels = extractSampleValuesFromImages(sampleImages)
 
 export function gSolveOneChannel (channel: Matrix, shutterSpeed: number[], smoothness: number, samples: number, imageAmount: number): {g: number[], LE: number[]} {
   const n = 255
@@ -184,27 +130,6 @@ export function gsolveImage (
   }
 }
 
-// export type image = [Matrix, Matrix, Matrix]
-
-// ImageR
-// 1 2 3 4
-// 5 6 7 8
-
-// Image
-//      firrst pixel ................... last pixel
-// r        55                             24
-// g        222                            45
-// b        54                             200
-
-// RadianceMap -> one channel
-//          firrst image ................... last image
-// sample1       23                             45
-// sample2       68                             90
-// .
-// .
-// .
-// sample_last   111                             125
-
 export function getActualRadiance (
   images: Matrix[],
   radianceMaps: [Matrix, Matrix, Matrix],
@@ -213,35 +138,29 @@ export function getActualRadiance (
   const rows = images[0].rows
   const columns = images[0].columns
   const numImages = images.length
-  // const channels = 3
 
   const denominator = Matrix.zeros(rows, columns)
   const numerator = Matrix.zeros(rows, columns)
   const arImage = Matrix.zeros(rows, columns)
-
-  // for (let i = 0; i < numImages; i++) { // 1
-
   const wij = [Matrix.zeros(rows, columns), Matrix.zeros(rows, columns), Matrix.zeros(rows, columns)] // 3
 
   let numberOfLogs = 0
   const gZij = [Matrix.zeros(rows, columns), Matrix.zeros(rows, columns), Matrix.zeros(rows, columns)]
 
+  // 4
   for (let i = 0; i < numImages; i++) { // 3
-    const zij = images[i] // 2
-    // gZij.setRow(channel, radianceMaps[channel].sub(shutterSpeed[i])) // 4
     for (let column = 0; column < columns; column++) {
       for (let row = 0; row < rows; row++) {
         gZij[i].set(
           row,
           column,
           radianceMaps[i].get(0, images[i].get(row, column)) - shutterSpeed[i]
-        ) // 4
+        )
       }
     }
   }
 
-  // gZij = gZij.sub(shutterSpeed[i]) // 5
-
+  // 5
   // apply weight
   for (let channel = 0; channel < numImages; channel++) {
     for (let column = 0; column < columns; column++) {
@@ -250,8 +169,8 @@ export function getActualRadiance (
       }
     }
   }
-  // numerator = numerator.add(wij.mul(zij)) // 7
 
+  // 7
   for (let channel = 0; channel < numImages; channel++) {
     for (let column = 0; column < columns; column++) {
       for (let row = 0; row < rows; row++) {
@@ -264,9 +183,6 @@ export function getActualRadiance (
   for (let channel = 0; channel < numImages; channel++) {
     denominator.add(wij[channel]) // 8
   }
-  // }
-  // arImage = numerator.div(denominator)
-  // elementwise matrix division
 
   denominator.add(1)
 
@@ -285,29 +201,6 @@ export function getActualRadiance (
 
   return arImage
 }
-
-// // check if nan
-// if (isNaN(radianceMaps[c].get(0, zij.get(r, j))) && numberOfLogs < 10) {
-//   console.log('nan', zij.get(r, j))
-//   numberOfLogs++
-// }
-
-// apply weight
-// zij.set(
-//   r,
-//   j,
-//   weight(zij.get(r, j))
-// )
-// console.log(r, j, numerator[i].get(r, j) + weight(zij.get(r, j)) * zij.get(r, j))
-
-// apply weight
-// zij[c] = new Matrix([zij[c].to1DArray().map(val => weight(val))])
-// const gZijArray = gZij[c].to1DArray()
-// const arr = zij[c].to1DArray().map(val => weight(val))
-// const mult = arr.map((z, i) => z * gZijArray[i])
-// finalImages[c] = (numerator[c].to1DArray().map((val, i) => val + mult[i])).map((val, i) => val / denominator[c].getColumn(i)[0])
-
-// numerator[c] = numerator[c].add(zij[c].mmul(g_zij[c].transpose()))
 
 export function ToneMapping (
   img: [Matrix, Matrix, Matrix]
